410988036ca9bf5d16271f6695820fd4f7a1ede1
Anna Rempe <annarempe@nalsanchez.corporate.thoughtworks.com>
2019-08-13 14:42:07 -0500

    Rempe [#1.1] added welcome msg

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        displayWelcomeMsg();
+    }
+
+    public static void displayWelcomeMsg(){
+        System.out.println("Welcome to Biblioteca. " +
+                "You're one-stop-shop for great book titles in Bangalore!");
     }
 }

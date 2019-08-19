0e53095e4fd05b7b4bbfcaca54244ce042c4434e
Anna Rempe <anna.rempe@thoughtworks.com>
2019-08-19 14:38:53 -0500

    Rempe book extending item

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,49 +1,21 @@
 package com.twu.biblioteca;
 
-import javax.print.MultiDocPrintService;
-import java.io.PrintStream;
-
-public class Book {
-    private String title;
+public class Book extends Item {
     private String author;
     private int pub_year;
-    private boolean checked_in;
-    private String checked_out_by;
 
     public Book(String title, String author, int pub_year){
-        this.title = title;
+        super(title);
         this.author = author;
         this.pub_year = pub_year;
-        this.checked_in = true;
-        this.checked_out_by = null;
     }
 
-    public String getTitle(){
-        return this.title;
-    }
 
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public boolean getStatus(){
-        return this.checked_in;
-    }
-
-    public void setStatus(boolean status){
-        this.checked_in = status;
-    }
 
     public String toString(){
         return String.format("%-20s\t%-20s\t%-5d",
         this.title, this.author, this.pub_year);
     }
 
-    public String getCheckedOutBy(){
-        return this.checked_out_by;
-    }
 
-    public void setCheckedOutBy(String usr_lib_num) {
-        this.checked_out_by = usr_lib_num;
-    }
 }
---
+++ src/com/twu/biblioteca/Item.java
@@ -0,0 +1,34 @@
+package com.twu.biblioteca;
+
+public class Item {
+    protected String title;
+    protected boolean status;
+    protected String checked_out_by;
+
+    protected Item(String title){
+        this.title = title;
+        this.status = true;
+        this.checked_out_by = null;
+    }
+
+    protected String getTitle(){
+        return this.title;
+    }
+
+    protected boolean getStatus(){
+        return this.status;
+    }
+
+    protected void setStatus(boolean status){
+        this.status = status;
+    }
+
+    protected String getCheckedOutBy(){
+        return this.checked_out_by;
+    }
+
+    protected void setCheckedOutBy(String usr_lib_num) {
+        this.checked_out_by = usr_lib_num;
+    }
+
+}
